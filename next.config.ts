494bc58c86d423b22db024ac48ef42f68f81802a
Anish Bharadwaj <bharadwajanish@gmail.com>
2026-01-23 02:49:13 -0800

    fix: Edited config so it doesnt have allowDevorigins

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
-const allowedDevOrigins = (process.env.ALLOWED_DEV_ORIGINS ||
-  process.env.APP_BASE_URL ||
-  process.env.BETTER_AUTH_URL ||
-  "http://localhost:3000")
-  .split(",")
-  .map((origin) => origin.trim())
-  .filter(Boolean);
+// const allowedDevOrigins = (process.env.ALLOWED_DEV_ORIGINS ||
+//   process.env.APP_BASE_URL ||
+//   process.env.BETTER_AUTH_URL ||
+//   "http://localhost:3000")
+//   .split(",")
+//   .map((origin) => origin.trim())
+//   .filter(Boolean);
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins,
+  // allowedDevOrigins,
 };
 
 export default nextConfig;
